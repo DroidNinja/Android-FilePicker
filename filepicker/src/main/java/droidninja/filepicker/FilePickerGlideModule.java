313a63942873fa80a03e067f8cab29c10c132441
droidNinja <arun2007ind@gmail.com>
2017-08-13 14:47:13 +0530

    updated glide version

---
+++ filepicker/src/main/java/droidninja/filepicker/FilePickerGlideModule.java
@@ -0,0 +1,7 @@
+package droidninja.filepicker;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+   
+@GlideModule
+public final class FilePickerGlideModule extends AppGlideModule {}
